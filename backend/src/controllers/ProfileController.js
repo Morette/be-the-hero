b830237da52f88af3cc3cff457c27cd36c3c3151
Thiago Morette <thiagomorette@gmail.com>
2020-05-22 20:35:45 +0100

    added validation with celebrate()

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,7 +1,7 @@
 const connection = require('../database/connection');
 
 module.exports = {
-	async listIncidents(req, res) {
+	async index(req, res) {
 		const ong_id = req.headers.authorization;
 
 		const incidents = await connection('incidents')
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const SessionController = require('./controllers/SessionController');
 const OngController = require('./controllers/OngController');
@@ -10,12 +11,50 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.listOngs);
-routes.post('/ongs', OngController.createOng);
+routes.post(
+	'/ongs',
+	celebrate({
+		[Segments.BODY]: Joi.object().keys({
+			name: Joi.string().required(),
+			email: Joi.string().required().email(),
+			whatsapp: Joi.number().required().min(9),
+			city: Joi.string().required(),
+			uf: Joi.string().required().length(2),
+		}),
+	}),
+	OngController.createOng,
+);
 
-routes.get('/incidents', IncidentsController.listIncidents);
+routes.get(
+	'/incidents',
+	celebrate({
+		[Segments.QUERY]: Joi.object().keys({
+			page: Joi.number(),
+		}),
+	}),
+	IncidentsController.listIncidents,
+);
+
 routes.post('/incidents', IncidentsController.create);
-routes.delete('/incidents/:id', IncidentsController.delete);
 
-routes.get('/profile', ProfileController.listIncidents);
+routes.delete(
+	'/incidents/:id',
+	celebrate({
+		[Segments.PARAMS]: Joi.object().keys({
+			id: Joi.number().required(),
+		}),
+	}),
+	IncidentsController.delete,
+);
+
+routes.get(
+	'/profile',
+	celebrate({
+		[Segments.HEADERS]: Joi.object({
+			authorization: Joi.string().required(),
+		}).unknown(),
+	}),
+	ProfileController.index,
+);
 
 module.exports = routes;
